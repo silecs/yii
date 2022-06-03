7d1ac13fa5298913651d07f327b92d69d2513c85
François Gannaz <francois.gannaz@silecs.info>
2022-06-03 12:10:57 +0200

    CTreeView: remove commented code

--- framework/web/js/source/jquery.treeview.async.js
+++ framework/web/js/source/jquery.treeview.async.js
@@ -53,36 +53,6 @@ function load(settings, root, child, container) {
 	        $(container).treeview({add: child});
 	    }
 	}, settings.ajax));
-	/*
-	$.getJSON(settings.url, {root: root}, function(response) {
-		function createNode(parent) {
-			var current = $("<li/>").attr("id", this.id || "").html("<span>" + this.text + "</span>").appendTo(parent);
-			if (this.classes) {
-				current.children("span").addClass(this.classes);
-			}
-			if (this.expanded) {
-				current.addClass("open");
-			}
-			if (this.hasChildren || this.children && this.children.length) {
-				var branch = $("<ul/>").appendTo(current);
-				if (this.hasChildren) {
-					current.addClass("hasChildren");
-					createNode.call({
-						classes: "placeholder",
-						text: "&nbsp;",
-						children:[]
-					}, branch);
-				}
-				if (this.children && this.children.length) {
-					$.each(this.children, createNode, [branch])
-				}
-			}
-		}
-		child.empty();
-		$.each(response, createNode, [child]);
-        $(container).treeview({add: child});
-    });
-    */
 }
 
 var proxied = $.fn.treeview;
