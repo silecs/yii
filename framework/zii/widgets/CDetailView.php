056194213cd2a851f08d2f56a66e74fe867f38b5
François Gannaz <francois.gannaz@silecs.info>
2026-04-09 21:04:21 +0200

    CDetailView should never treat string values as callables
    When a value is also the name of a function, e.g.
    "Time" or "DATE", then instead of displaying it,
    CDetailView called it as a PHP function.
    
    This behavior was always dangerous, but at least it
    was arguably useful with PHP5 which lacks anonymous
    functions.
    With modern PHP, even PHP7, it was just error prone.

--- framework/zii/widgets/CDetailView.php
+++ framework/zii/widgets/CDetailView.php
@@ -211,7 +211,7 @@ class CDetailView extends CWidget
 			if(!isset($attribute['type']))
 				$attribute['type']='text';
 			if(isset($attribute['value']))
-				$value=is_callable($attribute['value']) ? call_user_func($attribute['value'],$this->data) : $attribute['value'];
+				$value=!is_string($attribute['value']) && is_callable($attribute['value']) ? call_user_func($attribute['value'],$this->data) : $attribute['value'];
 			elseif(isset($attribute['name']))
 				$value=CHtml::value($this->data,$attribute['name']);
 			else
